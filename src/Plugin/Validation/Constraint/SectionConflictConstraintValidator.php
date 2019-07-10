e183c6f963d7cf09f1141abf0a21d735666d6b8c
Philipp Melab <philipp.melab@gmail.com>
2019-07-10 08:15:05 +0200

    Fixed error message.

--- src/Plugin/Validation/Constraint/SectionConflictConstraintValidator.php
+++ src/Plugin/Validation/Constraint/SectionConflictConstraintValidator.php
@@ -9,11 +9,11 @@ class SectionConflictConstraintValidator extends ConstraintValidator {
 
   public function validate($value, Constraint $constraint) {
     $dom = new \DOMDocument();
+    libxml_use_internal_errors(true);
     $dom->loadHTML($value);
     $xpath = new \DOMXPath($dom);
     $conflicts = $xpath->query('//*[local-name() = \'ck-conflict-text\' or local-name() =\'ck-conflict-media\' or (local-name() = \'ck-button\' and @left)]')->count();
     if ($conflicts > 0) {
-      $this->context->addViolation($constraint->containsConflicts, ['%count' => $conflicts]);
       $this->context->buildViolation($constraint->containsConflicts)
         ->setParameter('%count', $conflicts)
         ->setPlural($conflicts)
